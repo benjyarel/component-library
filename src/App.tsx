0f5c12781a0f54f826d02f18c3befe5e5042f72d
Benjamin <b.leray35@gmail.com>
2023-09-24 09:53:03 +0200

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
- Let's get Started.
+      Let's get Started.
     </>
   )
 }
