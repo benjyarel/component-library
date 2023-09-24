fda797f8e70d798ca93659a823ea2bb5c8dc2331
Benjamin <b.leray35@gmail.com>
2023-09-24 16:04:43 +0200

    first elements of layout

--- src/Application/Layout/Layout.tsx
+++ src/Application/Layout/Layout.tsx
@@ -1,18 +1,41 @@
 import type { ReactNode } from "react";
-
+import styled from "styled-components";
 import { Header } from "../Header";
 import { useRecoilValue } from "recoil";
 import { isMenuOpen } from "../state";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  const result = useRecoilValue(isMenuOpen);
-  console.log(result);
+  const isMenuOpenState = useRecoilValue(isMenuOpen);
 
   return (
-    <div>
+    <>
       <Header />
-      <div>TINTIN</div>
-      {children}
-    </div>
+      <StyledLayout>
+        {isMenuOpenState && <MenuPanel />}
+        {children}
+      </StyledLayout>
+    </>
   );
 };
+
+const MenuPanel = () => {
+  return <MenuPanelBox>COUCOU</MenuPanelBox>;
+};
+
+const StyledLayout = styled.div`
+  display: flex;
+  // 100 view height - Header height - bottom border size
+  height: calc(100vh - 72px - 6px);
+  background: #e8e6e6;
+  border-left: solid 6px #000;
+  border-bottom: solid 6px #000;
+  border-right: solid 6px #000;
+`;
+
+const MenuPanelBox = styled.div`
+  width: 240px;
+  height: 100%;
+  background: #fff;
+
+  border-right: solid 6px #000;
+`;
