7b8fd478a4206acccf88216ab0460492122476b1
Benjamin <b.leray35@gmail.com>
2023-10-29 14:05:04 +0100

    first attempt of config elements

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -3,6 +3,16 @@ import { Layout } from "./components/Layout";
 import { RecoilRoot } from "recoil";
 import { components } from "../library";
 function App() {
+  console.log(components);
+
+  const createList = () => {
+    return components.map(({ id, componentName, category }) => {
+      return { id, componentName, category };
+    });
+    // groupBy categories to do
+  };
+
+  console.log("createList", createList());
   return (
     <RecoilRoot>
       <Layout>Hello WORLD</Layout>
--- src/library/DummyComponent/index.ts
+++ src/library/DummyComponent/index.ts
@@ -1,11 +1,20 @@
 
 
+import { atom } from 'recoil';
 import  { DummyComponent } from './DummyComponent';
 
+const ID = "dummy-component"
+
 export const config = {
+  id: ID,
   componentName: 'Dummy Component',
   category: "buttons",
-  component: DummyComponent,
+  atom: atom({
+    key: ID,
+    default: {
+      component: DummyComponent,
+    }
+  })
 }
 
 export default config;
