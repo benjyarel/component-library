492ad91e8e65f63e56978ab711121b366e1a08a3
Benjamin <b.leray35@gmail.com>
2023-10-29 13:38:34 +0100

    manual loader config

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -1,7 +1,7 @@
 import { Layout } from "./components/Layout";
 
 import { RecoilRoot } from "recoil";
-
+import { components } from "../library";
 function App() {
   return (
     <RecoilRoot>
--- src/library/DummyComponent/index.ts
+++ src/library/DummyComponent/index.ts
@@ -3,6 +3,9 @@
 import  { DummyComponent } from './DummyComponent';
 
 export const config = {
-  componentName: 'DummyComponent',
+  componentName: 'Dummy Component',
+  category: "buttons",
   component: DummyComponent,
 }
+
+export default config;
